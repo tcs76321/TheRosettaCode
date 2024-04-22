0d0f0cefea0f2f0e67ec033e9f79bf106f919515
Trevor Stahl <trevor@trevorstahl.com>
2024-04-21 21:39:14 -0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module is a simple demonstration of a Python script that prints "Hello, World!" to the standard output.
+It includes a main function and checks if the script is run as the main module.
+"""
+
+def hello_world():
+    """
+    This function prints "Hello, World!" to the standard output.
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    # Call the function only if this module is executed as the main script.
+    hello_world()
