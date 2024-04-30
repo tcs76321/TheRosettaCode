aa064f10aa0825ce6c5bc85fc639ff3f4d915ebe
Trevor Stahl <trevor@trevorstahl.com>
2024-04-30 13:32:19 -0500

    javascript, tscompiled from ts hello world

---
+++ HelloWorld/hello-world.js
@@ -0,0 +1,9 @@
+// This is a simple TypeScript program that prints "Hello, World!" to the console.
+// Function to display a message
+function displayMessage(message) {
+    console.log(message);
+}
+// Main message
+var greeting = "Hello, World!";
+// Call the function with the greeting
+displayMessage(greeting);
