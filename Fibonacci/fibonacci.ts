b8f9dda30ab480e1c9238dda86c8cf0236783fc3
Trevor Clark Stahl <trevor@trevorstahl.com>
2024-09-24 16:52:47 -0500

    Create fibonacci.ts

---
+++ Fibonacci/fibonacci.ts
@@ -0,0 +1,12 @@
+// Fibonacci sequence to Nth
+
+// Function to display a message
+function fib(n: int): void {
+    
+}
+
+// Main message
+const n: int = 10;
+
+// Call the function with the greeting
+fib(n);
